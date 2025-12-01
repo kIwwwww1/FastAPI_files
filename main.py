ceed53df1156d67685d4e64adf998f8781d6f99f
Данил Кравченко <danil29070@gmail.com>
2025-12-01 14:20:56 +0300

    Работа с файлами в FastAPI

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse, FileResponse
 
 app = FastAPI()
 
@@ -19,6 +20,20 @@ async def upload_files(upload_files: list[UploadFile]):
         with open(f'{index}_{filename}', 'wb') as f:
             f.write(file.read())
 
+@app.get('/files/{filename}')
+async def get_file(filename: str):
+    return FileResponse(filename)
+
+
+def iterfile(filename):
+    with open(filename, 'rb') as file:
+        while chunk := file.read(1024 * 1024):
+            yield chunk
+
+
+@app.get('/files/streaming/{filename}')
+async def get_streamig_file(filename: str):
+    return StreamingResponse(iterfile(filename), media_type='video/mp4')
 
 if __name__ == '__main__':
     uvicorn.run('main:app', reload=True)
